b8044537b9e78c50da8bb3011ade14ee4ed0d6b1
johnnydevsolutions <johnnydev.solutions@gmail.com>
2025-01-19 10:25:54 -0300

    ♻️ Refactor: remove unused component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,13 +6,11 @@
 import { defineComponent } from 'vue';
 
 // Components
-import HelloWorld from '../components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
-    HelloWorld,
   },
 });
 </script>
